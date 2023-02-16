78e3284015007922f016d2ffcc53cdd9376293ab
smessie <smessie@smessie.com>
2023-02-16 12:04:41 +0100

    feat: Implement login and logout

--- src/components/FormViewer.vue
+++ src/components/FormViewer.vue
@@ -1,18 +1,89 @@
 <template>
   <MDBContainer>
     <h1>FormViewer</h1>
+
+    <MDBCard>
+      <MDBCardBody class="w-100">
+        <MDBCardTitle>Authentication</MDBCardTitle>
+        <MDBCardText>
+          <div v-if="loggedIn">
+            <p class="text-success">
+              You are logged in as <em>{{ loggedIn }}</em
+              >.
+            </p>
+            <MDBBtn color="primary" @click="logout">Logout</MDBBtn>
+          </div>
+          <div v-else>
+            <MDBInput v-model="oidcIssuer" label="OIDC Issuer" type="text" required />
+            <small class="text-danger" v-if="authError">{{ authError }}<br /></small>
+            <MDBBtn color="primary" @click="login" style="margin-top: 1rem">Login</MDBBtn>
+          </div>
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
   </MDBContainer>
 </template>
 
 <script>
-import { MDBContainer } from "mdb-vue-ui-kit";
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardText, MDBCardTitle, MDBContainer, MDBInput } from "mdb-vue-ui-kit";
+import { getDefaultSession, handleIncomingRedirect, login, logout } from "@inrupt/solid-client-authn-browser";
 
 export default {
   name: "FormViewer",
   components: {
     MDBContainer,
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBInput,
+    MDBBtn,
+  },
+  data() {
+    return {
+      oidcIssuer: "",
+      authError: null,
+      loggedIn: null,
+    };
+  },
+  created() {
+    handleIncomingRedirect({
+      restorePreviousSession: true,
+    }).then((info) => {
+      this.loggedIn = info.webId;
+    });
+  },
+  methods: {
+    async login() {
+      await handleIncomingRedirect();
+
+      // 2. Start the Login Process if not already logged in.
+      if (!getDefaultSession().info.isLoggedIn) {
+        await login({
+          // Specify the URL of the user's Solid Identity Provider;
+          // e.g., "https://login.inrupt.com".
+          oidcIssuer: this.oidcIssuer,
+          // Specify the URL the Solid Identity Provider should redirect the user once logged in,
+          // e.g., the current page for a single-page app.
+          redirectUrl: window.location.href,
+          // Provide a name for the application when sending to the Solid Identity Provider
+          clientName: "FormViewer",
+        }).catch((e) => {
+          this.authError = e.message;
+        });
+      }
+    },
+    async logout() {
+      await logout();
+      this.loggedIn = undefined;
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+}
+</style>
