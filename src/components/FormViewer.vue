d32839d9ed78c767a17f38aceaaee4becffbb07d
smessie <smessie@smessie.com>
2023-02-16 11:35:03 +0100

    chore: Remove unneeded files

---
+++ src/components/FormViewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <MDBContainer>
+    <h1>FormViewer</h1>
+  </MDBContainer>
+</template>
+
+<script>
+import { MDBContainer } from "mdb-vue-ui-kit";
+
+export default {
+  name: "FormViewer",
+  components: {
+    MDBContainer,
+  },
+};
+</script>
+
+<style scoped></style>
