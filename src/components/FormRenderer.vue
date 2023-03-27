f48af140e85bb02d545a4a716234ff0d4d51d999
smessie <smessie@smessie.com>
2023-03-27 14:09:11 +0200

    chore: Add fork me on GitHub ribbon

--- src/components/FormRenderer.vue
+++ src/components/FormRenderer.vue
@@ -1,4 +1,14 @@
 <template>
+  <a href="https://github.com/smessie/FormRenderer"
+  ><img
+      loading="lazy"
+      width="149"
+      height="149"
+      src="/forkme_right_gray.png"
+      class="attachment-full size-full fork"
+      alt="Fork me on GitHub"
+      data-recalc-dims="1"
+  /></a>
   <MDBContainer>
     <h1>FormRenderer</h1>
 
@@ -515,5 +525,10 @@ h1 {
 }
 .card {
   margin-bottom: 2rem;
+}
+
+.fork {
+  float: right;
+  margin-top: -3em;
 }
 </style>
